0b2edd1be2ff94f06c8cae2f52ecaa69454bdb12
Kevin Lablona <lablona.kevin666@gmail.com>
2022-11-01 18:03:03 -0300

    Proyecto 1

---
+++ js/index.js
@@ -0,0 +1,31 @@
+//comentario de linea
+/* 
+de bloque 
+*/
+let edad = 5;
+console.log(edad);
+
+let numeroUno = 10;
+let numeroDos = 20;
+
+let resultadoSuma= 10 + 20;
+
+console.log("resultado de suma", resultadoSuma);
+
+let nombre = "Kevin";
+let apellido = "Lablona";
+let nombreCompleto = `Hola ${nombre} ${apellido} bienvenido`;
+console.log(nombreCompleto);
+
+let nombreUsuario = prompt("Ingrese su nombre");
+console.log(nombreUsuario);
+alert ("Hola " + nombreUsuario + ".")
+
+// let nombre1 = "Kevincio";
+
+ /*let nombre = prompt ("Ingrese su nombre");
+ alert("bienvenido");*/
+
+/* if(nombre === "Kevincio") {
+    alert("Gato");
+ }*/
---
+++ js/indexx2.js
@@ -0,0 +1,48 @@
+/*let edad = prompt("Ingrese su edad");
+
+if (edad >= 18) {
+    alert("Sos mayor de edad.");
+    
+} else {
+    alert("Sos menor de edad.")
+}*/
+
+/*let hora = prompt("Ingrese la hora");
+
+if (hora >= 6 && hora < 12) {
+    alert ("Buenos días!");
+} else if (hora >=12 && hora < 20) {
+    alert("Buenas tardes!")
+} else {
+    alert ("Buenas noches!")
+}*/
+
+/*for (let x = 0; x < 10; x++) {
+    console.log("Ciclo numerico" + x);
+}*/
+
+/*let Semana = Number (prompt("Ingrese el numero"))
+
+switch (Semana) {
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miercoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    case 6:
+        console.log("Sabado");
+        break;
+    case 7:
+        console.log("Domingo");
+        break;
+}*/
